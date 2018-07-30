4cda7e54e0a78d930652bc68f8ffd85f096d7579
461045246@QQ.com <461045246@qq.com>
2018-07-30 10:13:58 +0800

    第一希提交

---
+++ pages/details/details.js
@@ -0,0 +1,11 @@
+//details.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    
+  },
+  onLoad: function () {
+    
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,12 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    hotList:['1','2'],
+    footerList:['1','2','3','4']
+  },
+  //事件处理函数
+ 
+})
